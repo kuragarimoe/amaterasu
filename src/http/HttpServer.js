ba76c4bf490ab620f68d607281d4bb0683aec3d4
Ausakura <17104706+Ausakura@users.noreply.github.com>
2021-01-12 04:52:27 -0600

    leftover pain

--- src/http/HttpServer.js
+++ src/http/HttpServer.js
@@ -1,6 +1,8 @@
 const http = require("http");
 const fs = require("fs");
 const path = require("path");
+const { URL } = require("url");
+
 const Request = require("../structures/Request");
 const Response = require("../structures/Response");
 
@@ -27,43 +29,60 @@ class HttpServer {
         })(this, path.join(__dirname, "/../../routers"));
 
         // server
-        this._server = http.createServer((req, res) => {
-            this._handle(req, res)
+        this._server = http.createServer(async(req, res) => {
+            await this._handle(req, res)
         })
 
         // listen
         this._server.listen(3821);
     }
 
-    _handle(req, res) {
+    async _handle(req, res) {
+        // handle qs
+        let url = new URL(req.url, `https://${req.headers.host}`);
+        req.path = url.pathname;
+
         // get router
         let router = this._routers.find((r) => {
             if (r._domain instanceof RegExp) {
                 return r._domain.test(req.headers.host);
             } else return r._domain == req.headers.host;
         });
+
         if (!router) router = this._routers[0]; // _default
 
         let route = router.routes.find((r) => {
             if (r.path instanceof RegExp) {
-                return r.path.test(req.url) && r.methods.includes(req.method);
-            } else return r.path == req.url && r.methods.includes(req.method);
+                return r.path.test(req.path) && r.methods.includes(req.method);
+            } else return r.path == req.path && r.methods.includes(req.method);
         });
 
         if (!route) {
             return res.end("Not Found.");
         }
+
+        // hnadle requests
         let _req = new Request(req);
         let _res = new Response(res);
 
-        req.on("data", chunk => {
-            _req.packets.push(chunk);
-            
-            // handle then send
-            _req._handle(); 
-            route.callback(_req, _res)
-        })
+        // set data
+        _req.query = url.searchParams;
+        _req.querystring = url.search;
 
+        let handled = false;
+        req.on("data", async chunk => {
+            _req.packets.push(chunk);
+
+            // handle then send
+            handled = true;
+            _req._handle();
+            route.callback(_req, _res);
+        });
+
+        setTimeout(() => {
+            if (!handled)
+                route.callback(_req, _res);
+        }, 200);
     }
 }
 
