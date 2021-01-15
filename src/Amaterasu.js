9d6592778f0d814dafc5928dad35f1b1e888fee1
Ausakura <17104706+Ausakura@users.noreply.github.com>
2021-01-14 19:39:49 -0600

    better loading

--- src/Amaterasu.js
+++ src/Amaterasu.js
@@ -1,9 +1,16 @@
 const HttpServer = require("./http/HttpServer");
+const Player = require("./structures/bancho/Player");
 
 class Amaterasu {
     constructor() {
         /// ASSIGN SERVERS ///
         this.http = new HttpServer(this);
+
+        /// BOT ///
+        glob.db.fetch("SELECT * FROM users WHERE 1").then((p) => {
+            glob.bot = new Player(p);
+            glob.players.add(glob.bot);
+        })
     }
 }
 
