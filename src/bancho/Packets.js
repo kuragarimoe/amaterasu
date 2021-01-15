3fee84c976d2fac9bc309a7a92b36e41e508b441
Ausakura <17104706+Ausakura@users.noreply.github.com>
2021-01-14 19:41:01 -0600

    adtl. packet handlers

--- src/bancho/Packets.js
+++ src/bancho/Packets.js
@@ -1,6 +1,8 @@
 // Overall Packets.
 module.exports = {
+    0: require("./requests/ActionUpdate"),
     1: require("./requests/SendMessage"),
     2: require("./requests/Logout"),
-    3: require("./requests/UserStatusUpdate")
+    3: require("./requests/UserStatusUpdate"),
+    79: require("./requests/RecieveUpdates")
 }
