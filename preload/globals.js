50dbd797f35ec700e2bd1f5d1e79946fad4f21d8
Ausakura <17104706+Ausakura@users.noreply.github.com>
2021-01-14 19:40:10 -0600

    better loading

--- preload/globals.js
+++ preload/globals.js
@@ -12,41 +12,43 @@ let cfg = toml.parse(fs.readFileSync(__dirname + "/../config/config.toml"));
 // set global space (we like glob. not global.)
 global.glob = {};
 
-// database and config
-glob.config = cfg;
-glob.db = new DatabaseHandler(mysql.createPool(mysql_cfg.database));
+module.exports = (async () => {
+    // database and config
+    glob.config = cfg;
+    glob.db = new DatabaseHandler(mysql.createPool(mysql_cfg.database));
 
-// cache
-glob.cache = new CacheStorage();
+    // cache
+    glob.cache = new CacheStorage();
 
-// lists and structures
-glob.players = new PlayerMap();
+    // lists and structures
+    glob.players = new PlayerMap();
 
-// get a list of current roles
-(async() => {
-    let [roles, _] = await glob.db.execute("SELECT * FROM roles");
+    // get a list of current roles
+    (async () => {
+        let [roles, _] = await glob.db.execute("SELECT * FROM roles");
 
-    glob.roles = roles;
-})();
+        glob.roles = roles;
+    })();
 
-// get channels
-(async() => {
+    // get channels
     let [channels, _] = await glob.db.execute("SELECT * FROM channels");
 
     glob.channels = channels.map(c => new Channel(c));
-})();
 
-// bancho packets
-glob.packets = {};
+    // bancho packets
+    glob.packets = {};
 
-for (let file of fs.readdirSync(__dirname + "/../src/bancho/packets")) {
-    glob.packets[file.slice(0, -3).toLowerCase()] = require(__dirname + "/../src/bancho/packets/" + file);
-}
+    for (let file of fs.readdirSync(__dirname + "/../src/bancho/packets")) {
+        glob.packets[file.slice(0, -3).toLowerCase()] = require(__dirname + "/../src/bancho/packets/" + file);
+    }
 
-// handled
-glob.handled = [];
+    // handled
+    glob.handled = [];
 
-// cmyui-fy (debug)
-glob.to_python = (packet) => {
-    return packet.join("\\x").split("\\x").map(c => c = `\\x${parseInt(c).toString(16).padStart(2, "0")}`).join("");
-}
+    // cmyui-fy (debug)
+    glob.to_python = (packet) => {
+        return packet.join("\\x").split("\\x").map(c => c = `\\x${parseInt(c).toString(16).padStart(2, "0")}`).join("");
+    }
+
+    return;
+});
--- server.js
+++ server.js
@@ -4,5 +4,19 @@
     this file works as a bootstrapper to load the basic core components to the server,
     as long as all things are properly configured.
 */
-require("./preload/globals");
-require("./src/Amaterasu");
+(async () => { // ASYNC FUNCTION
+    /// GLOBALS ///
+
+    await require("./preload/globals")();
+
+    /// PRE-INIT ///
+    glob.version = glob.config._internal.version // take the internal version and set it
+
+    glob.mem_usage = () => {
+        return (Math.round((process.memoryUsage().heapUsed / 1024 / 1024) * 100) / 100) + "MB";
+    }
+
+    /// INIT ///
+
+    require("./src/Amaterasu");
+})()
